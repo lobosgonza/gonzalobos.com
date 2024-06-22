7b2b7206336f3f1b7bf9af6ac38f8fdbb5160a5e
lobosgonza <gonzalo.lobos.ramirez@gmail.com>
2024-06-22 18:30:27 -0400

    arreglo botones, cambio JSON a JS

--- public/index.js
+++ public/index.js
@@ -1,26 +1,23 @@
+import data from './data.js';
+
 var contenedorDom;
 var navContainer;
 var navigationBar;
 
-var aboutMe;
-var experience;
-var education;
-var skills;
-var contact;
+
+
+var start = data.start;
+var navBar = data.navBar;
+var aboutMe = data.aboutMe;
+var experience = data.experience;
+var education = data.education;
+var skills = data.skills;
+var contact = data.contact;
 
 function init() {
-    document.querySelector('#root').innerHTML = `
-        <div id="navbar" class="fade-out-active"></div>
-        <div id="dymanicDomContainer" class="container custom-container mt-5">
-            <div id="inicio" class="card mb-4">
-                <div class="card-body text-center">
-                    <h1 class="card-title">Gonzalo Lobos</h1>
-                    <p class="card-text lead">Ecommerce | Project Management | Web Development</p>
-                    <button id="startButton" type="button" class="btn btn-primary">Entrar</button>
-                </div>
-            </div>
-        </div>
-        <div id="nav" class="container custom-container mt-5"></div>`;
+    console.log("init start");
+
+    document.querySelector('#root').innerHTML = start;
 
     contenedorDom = document.querySelector('#dymanicDomContainer');
     navContainer = document.querySelector('#nav');
@@ -28,70 +25,37 @@ function init() {
 
 
 
-    fetch('data.json')
-        .then(response => response.json())
-        .then(data => {
-            aboutMe = data.aboutMe;
-            experience = data.experience;
-            education = data.education;
-            skills = data.skills;
-            contact = data.contact;
 
-            document.querySelector('#startButton').addEventListener('click', () => {
+    document.querySelector('#startButton').addEventListener('click', () => {
 
+        contenedorDom.classList.remove('fade-in')
+        // contenedorDom.classList.add('fade-out')
 
-                navigationBar.classList.remove("fade-out-active")
-                navigationBar.classList.add("fade-in")
+        navigationBar.innerHTML = navBar;
+        navigationBar.classList.add("fade-in")
+        navigationBar.classList.remove("fade-out-active")
 
 
+        setContent(aboutMe)
+        querySelectors()
 
-                navigationBar.innerHTML = `
-                    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-                        <div class="container custom-container">
-                            <a class="navbar-brand" id="aboutMe" href="#">Gonzalo Lobos</a>
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-                            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                                <ul class="navbar-nav">
-                                    <li class="nav-item">
-                                        <a class="nav-link" id="aboutMe" href="#">¿Quien Soy?</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" id="experience" href="#">Experiencia</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" id="education" href="#">Estudios</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" id="skills" href="#">Habilidades</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" id="contact" href="#">Contacto</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </nav>`;
 
-                cargarAboutMe();
+    });
 
-                querySelectors()
-            });
-        })
-        .catch(error => console.error('Error loading data:', error));
+
+
 }
 
 function querySelectors() {
 
-    function linkButtons(id, func) {
-        document.querySelectorAll(id).forEach((button) => { button.addEventListener('click', func) })
+    function linkButtons(id, data) {
+        document.querySelectorAll(id).forEach((button) => { button.addEventListener('click', () => setContent(data)) })
     }
-    linkButtons("#experience", cargarExperience)
-    linkButtons('#aboutMe', cargarAboutMe);
-    linkButtons('#education', cargarEducation);
-    linkButtons('#skills', cargarSkills);
-    linkButtons('#contact', cargarContact);
+    linkButtons('#experience', experience)
+    linkButtons('#aboutMe', aboutMe);
+    linkButtons('#education', education);
+    linkButtons('#skills', skills);
+    linkButtons('#contact', contact);
 }
 
 
@@ -100,56 +64,16 @@ function setContent(newContent) {
     const oldContent = contenedorDom.firstElementChild;
 
 
-    if (oldContent) {
-
-        oldContent.classList.add('fade-out');
-
-        oldContent.addEventListener('transitionend', () => {
-
-            contenedorDom.innerHTML = newContent;
-
-            const newElement = contenedorDom.firstElementChild;
-
-            querySelectors()
+    contenedorDom.innerHTML = newContent;
 
 
-        }, { once: true });
-    } else {
-
-        contenedorDom.innerHTML = newContent;
-        const newElement = contenedorDom.firstElementChild;
-        newElement.classList.add('fade-in');
-        querySelectors()
-
-
-    }
-
-
-
+    querySelectors()
 
 }
 
-function cargarExperience() {
-    console.log('experience activada');
-    setContent(experience);
 
-}
 
-function cargarAboutMe() {
-    setContent(aboutMe);
-}
 
-function cargarEducation() {
-    setContent(education);
-}
-
-function cargarSkills() {
-    setContent(skills);
-}
-
-function cargarContact() {
-    setContent(contact);
-}
 
 document.addEventListener('DOMContentLoaded', function () {
     init();
